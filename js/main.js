b5d1f6a85589c5bbf12b538d0cf8e6254fc6a4b2
Ai Yen Tam <aiyentam@Stephens-MacBook-Pro-64.local>
2018-01-14 21:53:42 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 var cards = ['queen','queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = 'queen';
-cardsInPlay.push(cardOne);
+var checkForMatch = function() {
 
-console.log("User flipped queen");
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.")
+		}
+	}
+};
 
-var cardTwo = 'king';
-cardsInPlay.push(cardTwo);
+var flipCard = function(cardID) {	
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
 
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.")
-}}
