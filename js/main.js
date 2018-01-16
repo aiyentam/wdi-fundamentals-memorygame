3d5198574c9458b97155462f3429ce56be6fe3e5
Ai Yen Tam <aiyentam@Stephens-MacBook-Pro-64.local>
2018-01-16 17:58:25 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen','queen', 'king', 'king'];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "image/queen=of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "image/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "image/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "image/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -12,9 +33,11 @@ var checkForMatch = function() {
 	}
 };
 
-var flipCard = function(cardID) {	
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+var flipCard = function(cardId) {	
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage); 
+	console.log(cards[cardId].suit);
 	checkForMatch();
 };
 
